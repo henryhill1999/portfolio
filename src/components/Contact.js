f31a2567cd93515f08469ddb427a26cb00e6917e
Henry Hill <henry.jacob.hill@gmail.com>
2020-09-25 23:34:46 -0400

    removed unused-vars warning

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,6 @@
 import React from "react";
-import { createUseStyles } from "react-jss";
-
-const useStyles = createUseStyles({});
 
 const About = (props) => {
-  const classes = useStyles();
-
   return (
     <>
       <h1>Hello!</h1>
